bc207824c4c2be5f8e3161bc1e15fca88342e4f2
Christian Fei <info@christian-fei.com>
2014-07-01 23:26:26 +0200

    init, readme, xpepperify.sh, progress, overlay alpha

---
+++ README.md
@@ -0,0 +1,17 @@
+#xpepperify
+
+[xpepperify](http://xpeppers.com) your avatar!
+
+# Steps
+
+1. clone this repo: `git clone --depth 1 https://github.com/christian-fei/xpepperify.git`
+
+2. grab your google plus profile picture
+
+3. name it **profile.jpg** and place it in the xpepperify folder
+
+3.1 `wget {URL} -O profile.png`
+
+4. `sh xpepperify.sh`
+
+5. profit
---
+++ xpepperify.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+
+function progress {
+	column_count=$(tput cols)
+	
+  for i in $(seq 1 $column_count);
+	do
+		printf "="
+		sleep .001
+	done
+	echo 
+}
+
+if [ ! -f ./profile.png ]; then
+	echo "required files are missing\n"
+	exit 1
+fi
+
+echo "processing images."
+
+progress
+
+# http://www.imagemagick.org/discourse-server/viewtopic.php?f=1&t=16335&start=0
+convert profile.png -resize 500x500 miff:- | composite -gravity center xpeppers-overlay.png - xpepperified.png
+
+echo "xpepperified.png has been created."
+
+open xpepperified.png
