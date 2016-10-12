3de29090c97c19fae41e1e250502da8485ece450
liuggio <liuggio@gmail.com>
2016-10-12 13:52:34 +0200

    Fixed Overlay, Readme and httpd

--- README.md
+++ README.md
@@ -7,31 +7,31 @@
 # Run
 
 - `docker build . -t xpepperify:latest`
-- `docker run -p "8080:8080" -v "`pwd`:/xpeppers"`
+- `docker run -p "8080:8080" -v "`pwd`:/xpeppers" xpepperify`
 
 # Usage
 
 You can use three overlays:
 
-1. http://server:port/url-of-the-image
-2. http://server:port/red/url-of-the-image
-3. http://server:port/white-on-red/url-of-the-image
+1. http://localhost:8080/url-of-the-image
+2. http://localhost:8080/red/url-of-the-image
+3. http://localhost:8080/white-on-red/url-of-the-image
 
 # Example
 
-*1. The red overlay 
+1. The red overlay 
 
-- `wget "http://localhost:8080/https://static.artfire.com/uploads/product/7/367/82367/6082367/6082367/large/troll_face_-_6_x_5_25_decal_18d24f31.jpg"`
+`wget "http://localhost:8080/https://static.artfire.com/uploads/product/7/367/82367/6082367/6082367/large/troll_face_-_6_x_5_25_decal_18d24f31.jpg"`
 
-*2. The white overlay 
+2. The white overlay 
 
-- `wget "http://localhost:8080/white/https://static.artfire.com/uploads/product/7/367/82367/6082367/6082367/large/troll_face_-_6_x_5_25_decal_18d24f31.jpg"`
+`wget "http://localhost:8080/white/https://static.artfire.com/uploads/product/7/367/82367/6082367/6082367/large/troll_face_-_6_x_5_25_decal_18d24f31.jpg"`
 
-*3. The white on red overlay  
+3. The white on red overlay  
 
-- `wget "http://localhost:8080/white-on-red/https://static.artfire.com/uploads/product/7/367/82367/6082367/6082367/large/troll_face_-_6_x_5_25_decal_18d24f31.jpg"`
+`wget "http://localhost:8080/white-on-red/https://static.artfire.com/uploads/product/7/367/82367/6082367/6082367/large/troll_face_-_6_x_5_25_decal_18d24f31.jpg"`
 
 # Dependencies
 
 - docker
-- job @ xpeppers
+- [job @ xpeppers](http://www.xpeppers.com/carriere)
--- http.sh
+++ http.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
 
+printHelp() {
+echo "HTTP/1.0 500 Problem with format
+
+<html>
+<body>
+<h1>Xpepperify your avatar</h1>
+<p>
+Usage for the <br>
+<ul><li><strong>red overlay</strong><img width=\"30\" src=\"/https://static.artfire.com/uploads/product/7/367/82367/6082367/6082367/large/troll_face_-_6_x_5_25_decal_18d24f31.jpg\"><a href="/https://static.artfire.com/uploads/product/7/367/82367/6082367/6082367/large/troll_face_-_6_x_5_25_decal_18d24f31.jpg">http://this-website-address.com/http://url-of-the-avatar.png</a>
+</li><li><strong>white overlay</strong><img width=\"30\" src=\"/white/https://static.artfire.com/uploads/product/7/367/82367/6082367/6082367/large/troll_face_-_6_x_5_25_decal_18d24f31.jpg\"><a href="/white/https://static.artfire.com/uploads/product/7/367/82367/6082367/6082367/large/troll_face_-_6_x_5_25_decal_18d24f31.jpg">http://this-website-address.com/white/http://url-of-the-avatar.png</a>
+</li><li><strong>white over red overlay</strong><img width=\"30\" src=\"/white-on-red/https://static.artfire.com/uploads/product/7/367/82367/6082367/6082367/large/troll_face_-_6_x_5_25_decal_18d24f31.jpg\"><a href="/white-on-red/https://static.artfire.com/uploads/product/7/367/82367/6082367/6082367/large/troll_face_-_6_x_5_25_decal_18d24f31.jpg">http://this-website-address.com/white-on-red/http://url-of-the-avatar.png</a>
+</li></ul>
+</p>
+<img  width=\"200\"  src=\"/https://static.artfire.com/uploads/product/7/367/82367/6082367/6082367/large/troll_face_-_6_x_5_25_decal_18d24f31.jpg\">
+</body>
+</html>
+";
+}
+
+
 assertRequestContainsPng() {
     file=$1
     if [[ ${file: -4} == ".png" ]]  ; then
@@ -22,7 +42,7 @@ response() {
     cat $output
 }
 
-tmpFile=$(mktemp /tmp/file.XXXXXXXXXX) || { echo "Failed to create temp file"; exit 1; }
+tmpFile=$(mktemp /tmp/file.XXXXXXXXXX) || { echo "HTTP/1.0 500 Problem with format";echo "Failed to create temp file"; exit 1; }
 output="$tmpFile.png"
 
 read -r line
@@ -32,6 +52,7 @@ read -r REQUEST_METHOD REQUEST_URI REQUEST_HTTP_VERSION <<<"$line"
 assertRequestContainsPng
 
 profile=${REQUEST_URI:1}; overlay="red"
+
 dev="white/";if [[ $profile == $dev* ]] ; then profile=${REQUEST_URI:2};overlay="white"; profile=${profile:${#overlay}};  fi;
 dev="white-on-red/";if [[ $profile == $dev* ]] ; then profile=${REQUEST_URI:2};overlay="white-on-red";profile=${profile:${#overlay}}; fi;
 overlay=$overlay.png
@@ -43,7 +64,11 @@ echo "> $REQUEST_URI" >&2;
 echo "> $REQUEST_HTTP_VERSION" >&2;
 echo "> profile $profile; $overlay" >&2;
 
-wget -o /dev/null -a /dev/null $profile -O $tmpFile || { echo "Failed to get the file"; exit 1; }
+wget -o /dev/null -a /dev/null $profile -O $tmpFile
+
+if [[ "$?" == "1" ]]; then
+    printHelp ; exit 1;
+fi
 convert $tmpFile -resize 500x500 miff:- | composite -gravity center overlay/$overlay - $output
 
 response $output
